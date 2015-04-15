b8a73bc8eb044d32671194e73c8a58759f7e28f4
Andrew Matuszewski <aMat@MacBook-Pro.local>
2015-04-15 01:03:05 -0700

    First dotfile commit

---
+++ symlink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+files=~/dotfiles/*    		  # list of files/folders to symlink in homedir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$(basename $file) ~/.$(basename $file)
+done
+
